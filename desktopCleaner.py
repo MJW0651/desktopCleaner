28c53c39807df0681be028313f00a01df147103f
MJW0651 <michael.woolcott96@gmail.com>
2024-01-26 17:21:09 -0600

    Description and (future) feature list.

---
+++ desktopCleaner.py
@@ -0,0 +1,21 @@
+""" 
+
+Desktop Organizer and Cleaner
+
+Collect and organize files on a user's desktop and organize
+them based on file type.
+
+- List files on the desktop, sorted by file type
+- Collect files into single folder
+    - Sub-folder for each 
+- Delete files of a certain type
+- Display how much space the files take up (MB/GB)
+    - Breakdown by file type
+
+"""
+
+
+
+
+
+
