9336eecde8d8a58fa5958163b6720eb5cbadc5e1
MJW0651 <michael.woolcott96@gmail.com>
2024-01-26 17:28:16 -0600

    List files on desktop function.

--- desktopCleaner.py
+++ desktopCleaner.py
@@ -14,6 +14,34 @@ them based on file type.
 
 """
 
+import os
+
+# List files function
+def list_files_on_desktop():
+    if os.name == 'posix': # Unix-based operating systems
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    elif os.name == 'nt': # Windows operating systems
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    else: 
+        return "Unsupported operating system."
+    
+    if os.path.exists(desktop_path):
+        files_on_desktop = os.listdir(desktop_path)
+        return files_on_desktop
+    else: 
+        return "Desktop directory not found."
+    
+files = list_files_on_desktop()
+if isinstance(files, list):
+    print("Files on desktop:")
+    for file in files:
+        print(file)
+
+else: 
+    print(files)
+
+
+                                    
 
 
 
